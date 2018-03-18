1f303da3390022aaffa6175cb88de4c9cbf8983f
zoheers <36574675+zoheers@users.noreply.github.com>
2018-03-18 13:54:36 +0300

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,29 @@
+# allowed papers: 100, 50, 10, 5, and rest of request
+
+money = 500
+request = 277
+
+def ATM(request):
+    while request > 0:
+        if request>100 :
+            print "give 100"
+            request-=100
+        else :
+            if request>50 :
+                print "give 50"
+                request-=50
+            else :
+                if request>10 :
+                    print "give 10"
+                    request-=10
+                else :
+                    if request>5 :
+                        print "give 5"
+                        request-=5
+                    else :
+                        print "give " , request
+                        request=0
+                        
+ATM (request)            
+                
+    
