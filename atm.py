28e20559a62019f88f0294de4adf680920e46b54
zoheers <36574675+zoheers@users.noreply.github.com>
2018-03-25 19:03:53 +0300

    Update atm.py

--- atm.py
+++ atm.py
@@ -3,27 +3,31 @@
 money = 500
 request = 277
 
-def ATM(request):
-    while request > 0:
-        if request>100 :
-            print "give 100"
-            request-=100
-        else :
-            if request>50 :
-                print "give 50"
-                request-=50
-            else :
-                if request>10 :
-                    print "give 10"
-                    request-=10
+def ATM(request,money):
+    if money > request :
+        money -= request
+        while request > 0:
+                if request>100 :
+                    print "give 100"
+                    request-=100
                 else :
-                    if request>5 :
-                        print "give 5"
-                        request-=5
+                    if request>50 :
+                        print "give 50"
+                        request-=50
                     else :
-                        print "give " , request
-                        request=0
-                        
-ATM (request)            
+                        if request>10 :
+                            print "give 10"
+                            request-=10
+                        else :
+                            if request>5 :
+                                print "give 5"
+                                request-=5
+                            else :
+                                print "give " , request
+                                request=0
+    print "money = " +str(money )
+
+                       
+ATM (request,money )            
                 
     
